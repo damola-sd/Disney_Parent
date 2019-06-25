49ad42d7f87ab6d5b51cdd3b4052d9ba1884b614
Damola <damolasd@gmail.com>
2019-06-25 13:13:28 +0100

    Adding action creators

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import {createStore, applyMiddleware} from 'redux';
 import { Provider } from 'react-redux';
 
 const store = createStore(
-    ,
     applyMiddleware(thunk, logger));
 
 ReactDOM.render(
